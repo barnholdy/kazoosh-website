298b55d3d1127c6153ca686b6233bce359735de4
Hannes Leitner <hannes.leitner@me.com>
2014-11-19 20:02:47 +0100

    added page name as class to body

--- public_html/js/controllers/MainCtrl.js
+++ public_html/js/controllers/MainCtrl.js
@@ -1,4 +1,7 @@
-kazoosh.controller('MainCtrl' ,function ($scope) {
+kazoosh.controller('MainCtrl', ['CONF', '$scope', '$state', function(CONF, $scope, $state) {
 
-	
-});
+	$scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+
+		$scope.pageName = toParams.type ? toParams.type : toState.name;
+	})
+}]);
